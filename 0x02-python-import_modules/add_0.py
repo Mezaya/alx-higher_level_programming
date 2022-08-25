b7bc3a04dc7dcadd753864dc8fcca8bbf79a8ec0
mezaya <mesaya@live.com>
2022-08-25 19:33:22 +0200

    Pythonicus Modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """The addition function
+
+    Arguments:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sum of values, a + b
+    """
+    return (a + b)
