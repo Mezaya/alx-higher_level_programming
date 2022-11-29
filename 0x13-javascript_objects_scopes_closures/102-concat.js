8fc3531c079b94e74042297ebb21826668e54d76
mezaya <mesaya@live.com>
2022-11-29 20:55:11 +0200

    O_S_C

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,30 @@
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+fs.stat(process.argv[2], function (error, stats) {
+  if (error) throw error;
+  fs.open(process.argv[2], 'r', function (error, fd) {
+    if (error) throw error;
+    fs.readFile(process.argv[2], function read (err, data) {
+      if (err) throw err;
+      let fa = data.toString('utf-8', 0, stats.size);
+      fs.writeFile(process.argv[4], fa, function (err) {
+        if (err) throw err;
+      });
+    });
+  });
+});
+
+fs.stat(process.argv[3], function (error, stats) {
+  if (error) throw error;
+  fs.open(process.argv[3], 'r', function (error, fd) {
+    if (error) throw error;
+    fs.readFile(process.argv[3], function read (err, data) {
+      if (err) throw err;
+      let fa = data.toString('utf-8', 0, stats.size);
+      fs.appendFile(process.argv[4], fa, function (err) {
+        if (err) throw err;
+      });
+    });
+  });
+});
